31162693d6d3299e00285ccbf3c175fba8465b4a
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2025-10-04 12:29:34 -0300

    feat: add drawer

--- Space-Apps-Challenge/src/App.tsx
+++ Space-Apps-Challenge/src/App.tsx
@@ -1,20 +1,48 @@
 import "maplibre-gl/dist/maplibre-gl.css";
 import Map from "react-map-gl/maplibre";
 import "./App.css";
+import { Button } from "./components/ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "./components/ui/drawer";
 
 function App() {
   // const [count, setCount] = useState(0);
 
   return (
-     <Map
-      initialViewState={{
-        longitude: -122.4,
-        latitude: 37.8,
-        zoom: 14
-      }}
-       style={{ width: "100vw", height: "100vh" }}
-      mapStyle="https://api.maptiler.com/maps/streets/style.json?key=nNpWDVPlrqIFXJhqS2Kw"
-    />
+    <>
+      <Drawer>
+        <DrawerTrigger>Open</DrawerTrigger>
+        <DrawerContent>
+          <DrawerHeader>
+            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
+            <DrawerDescription>This action cannot be undone.</DrawerDescription>
+          </DrawerHeader>
+          <DrawerFooter>
+            <Button>Submit</Button>
+            <DrawerClose>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+      <Map
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=nNpWDVPlrqIFXJhqS2Kw"
+      ></Map>
+    </>
   );
 }
 
