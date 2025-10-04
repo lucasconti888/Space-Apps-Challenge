26f3cde89db1782903d9d21db29cbc6b74260749
Lucas Conti <lucas.pereira@sou.inteli.edu.br>
2025-10-04 13:54:41 -0300

    feat: get info from places

--- Space-Apps-Challenge/src/App.tsx
+++ Space-Apps-Challenge/src/App.tsx
@@ -1,48 +1,44 @@
 import "maplibre-gl/dist/maplibre-gl.css";
-import Map, { Marker } from "react-map-gl/maplibre";
+import { useMemo, useState } from "react";
+import Map, {
+  Marker,
+  NavigationControl,
+  type MapLayerMouseEvent,
+} from "react-map-gl/maplibre";
 import "./App.css";
-import { Button } from "./components/ui/button";
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "./components/ui/drawer";
+import Sidebar from "./components/drawer";
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [clickedItem, setClickedItem] = useState<MapLayerMouseEvent>();
+  const [open, setOpen] = useState(false);
+
+  const item = useMemo(() => {
+    if (!clickedItem) return null;
+    return clickedItem.lngLat || null;
+  }, [clickedItem]);
 
   return (
     <>
-      <Drawer>
-        <DrawerTrigger>Open</DrawerTrigger>
-        <DrawerContent>
-          <DrawerHeader>
-            <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-            <DrawerDescription>This action cannot be undone.</DrawerDescription>
-          </DrawerHeader>
-          <DrawerFooter>
-            <Button>Submit</Button>
-            <DrawerClose>
-              <Button variant="outline">Cancel</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
+      <Sidebar item={item} open={open} setOpen={setOpen} />
       <Map
         initialViewState={{
           longitude: -122.4,
           latitude: 37.8,
           zoom: 14,
         }}
+        onClick={(e) => {
+          setClickedItem(e);
+          setOpen(true);
+        }}
         style={{ width: "100vw", height: "100vh" }}
         mapStyle="https://api.maptiler.com/maps/streets/style.json?key=nNpWDVPlrqIFXJhqS2Kw"
       >
-        <Marker longitude={16.62662018} latitude={49.2125578} />
+        <NavigationControl position="top-left" />
+        <Marker
+          longitude={16.62662018}
+          latitude={49.2125578}
+          color="#61dbfb"
+        ></Marker>
       </Map>
     </>
   );
---
+++ Space-Apps-Challenge/src/components/drawer/index.tsx
@@ -0,0 +1,75 @@
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Typography from "@mui/material/Typography";
+import { Drawer, DrawerContent, DrawerTrigger } from "../ui/drawer";
+
+interface SidebarProps {
+  item: {
+    [name: string]: any;
+  } | null;
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export default function Sidebar({ item, open, setOpen }: SidebarProps) {
+  return (
+    <Drawer open={open} onClose={() => setOpen(false)}>
+      <DrawerTrigger onClick={() => setOpen(true)}>Abrir</DrawerTrigger>
+
+      <DrawerContent>
+        {item ? (
+          <List>
+            <ListItem>
+              <ListItemText
+                primary={<Typography variant="h4">{item?.lng}</Typography>}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography variant="h5">Host: {item?.lat}</Typography>
+                }
+                secondary={<Typography>id: {item?.host_id}</Typography>}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography>
+                    <b>Room type:</b> {item?.room_type}
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography>
+                    <b>Minimum night:</b> {item?.minimum_nights}
+                  </Typography>
+                }
+              />
+            </ListItem>
+            <ListItem key={item.id}>
+              <ListItemText
+                primary={
+                  <Typography>
+                    <b>Neighbourhood: </b>
+                    {item.neighbourhood}, {item?.neighbourhood_group}
+                  </Typography>
+                }
+              />
+            </ListItem>
+          </List>
+        ) : (
+          <List>
+            <Typography component="p" sx={{ padding: 2 }}>
+              Click to point to see more details
+            </Typography>
+          </List>
+        )}
+      </DrawerContent>
+    </Drawer>
+  );
+}
